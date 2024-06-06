18276c87e33a99d7c45b7d440bc3059ddf8b9ec8
H-KAFKA <aliagaoq@gmail.com>
2024-06-06 10:57:06 -0500

    nuevo avance 02

--- procesar.php
+++ procesar.php
@@ -1,21 +1,28 @@
 <?php
 require_once './login.php';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $nombre = $_POST['nombreCompleto'];
-    $email = $_POST['email'];
-    $telefono = $_POST['telefono'];
-
-    $query = "INSERT INTO pasajeros (nombre, email, telefono) VALUES (:nombre, :email, :telefono)";
-
-    $comando = $pdo->prepare($query);
-    $comando->execute([
-        'nombre' => $nombre,
-        'email' => $email,
-        'telefono' => $telefono
-    ]);
-
-    echo "Registrado correctamente";
-}else{
-    echo "Error en el formulario";
+try {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $nombre = $_POST['nombreCompleto'];
+        $email = $_POST['email'];
+        $telefono = $_POST['telefono'];
+        $dni = $_POST['dni'];
+    
+        $query = "INSERT INTO pasajeros (nombre, email, telefono, dni) VALUES (:nombre, :email, :telefono, :dni)";
+    
+        $comando = $pdo->prepare($query);
+        $comando->execute([
+            'nombre' => $nombre,
+            'email' => $email,
+            'telefono' => $telefono,
+            'dni' => $dni
+        ]);
+    
+        echo "Registrado correctamente";
+        header("include: administrador.php");
+    }else{
+        echo "Error en el formulario";
+    }
+} catch (PDOException $e) {
+    echo "Error en enviar datos";
 }
